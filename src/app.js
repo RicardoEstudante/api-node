652282e0b61b7c5f55d146c28f4d7b50a2a5dbef
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-24 15:57:41 -0300

    ultimos commits

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const http = require("http");
 const express = require("express");
+const status = require("http-status");
 const spoilersRoute = require('./routes/spoiler');
-const sequelize = require('./database/database')
+const sequelize = require('./database/database');
 
 const app = express();
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-app.set("port", port);
 
 app.use(express.json());
 
@@ -16,20 +12,20 @@ app.use('/api', spoilersRoute);
 
 app.use((request, response, next) => {
     response.status(404).send("hello word!");
-})
+});
 
 // erros desconhecidos são renderizados para o cliente atraves de uma arquivo json
-app.use((error, request, response, next) =>{
+app.use((error, request, response, next) => {
     response.status(500).json({ error });
 });
 
 // sync recebe um objeto como parametro, para pegar o banco de dados de acordo com o model
-sequelize.sync({ force : true }).then(() => {});
+sequelize.sync({ force: true }).then(() => {
+    const port = process.env.PORT || 3000; // pegar a porta configurada como padrão || pegar a porta 3000
 
+    app.set("port", port);
 
-// const server = http.createServer(app);
+    const server = http.createServer(app);
 
-
-// server.listen(port, hostname, () => {
-//     console.log(`Servidor em execução em http://${hostname}:${port}/`);
-// });
+    server.listen(port);
+});
--- src/model/spoiler.js
+++ src/model/spoiler.js
@@ -7,33 +7,33 @@ const sequelize = require('../database/database');
 // dois parametros, primeiro String para nomear a tabela, segundo um Obj com instruções de com a tabela deve ser criada;
 
 const Spoiler = sequelize.define("spoiler", {
-    id: {
-        allowNull: false, // não pode ser falso
-        autoIncrement: true, // se auto-imcrementa
-        primaryKey: true, // chave - primaria da tabela
-        type: Sequelize.INTEGER // tipo inteiro
-      },
-      titulo: {
-        allowNull: false, // pode ser nullo
-        type: Sequelize.STRING(255),
-        validate: {
-          len: [2, 255] // pode conter de 2 até 255 caracteres
-        }
-      },
-      espoliador: {
-        allowNull: false,
-        type: Sequelize.STRING(40),
-        validate: {
-          len: [2, 40]
-        }
-      },
-      descricao: {
-        allowNull: false,
-        type: Sequelize.STRING(255),
-        validate: {
-          len: [2, 255]
-        }
-      }
+  id: {
+    allowNull: false, // não pode ser falso
+    autoIncrement: true, // se auto-imcrementa
+    primaryKey: true, // chave - primaria da tabela
+    type: Sequelize.INTEGER // tipo inteiro
+  },
+  titulo: {
+    allowNull: false, // pode ser nullo
+    type: Sequelize.STRING(255),
+    validate: {
+      len: [2, 255] // pode conter de 2 até 255 caracteres
+    }
+  },
+  espoliador: {
+    allowNull: false,
+    type: Sequelize.STRING(40),
+    validate: {
+      len: [2, 40]
+    }
+  },
+  descricao: {
+    allowNull: false,
+    type: Sequelize.STRING(255),
+    validate: {
+      len: [2, 255]
+    }
+  }
 });
 
 module.exports = Spoiler; //  exportando o Spoiler
