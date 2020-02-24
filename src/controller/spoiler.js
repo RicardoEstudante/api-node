6522a4a72fcf36e3110e5a97f7f5e231c2320173
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-24 14:07:30 -0300

    controller

--- src/controller/spoiler.js
+++ src/controller/spoiler.js
@@ -1,10 +1,11 @@
 const Spoiler = require("../model/spoiler");
 
 
-// pergando o parametro id na URL e armazenando na const id;
+// pegando o parametro id na URL e armazenando na const id;
 exports.buscarUm = (request, response, next) => {
     const id = request.params.id;
 
+// buscando registro no banco de dados
     Spoiler.findById(id)
     .then(spoiler => {
         if (spoiler) {
@@ -16,3 +17,79 @@ exports.buscarUm = (request, response, next) => {
     })
     .catch(error => next(error));
 };
+// listando todos os registros usando paginação para controlar
+exports.buscarTodos = (request, response, next) => {
+    let limite = parseInt(request.query.limite || 0)
+    let pagina = parseInt(request.query.pagina || 0)
+ 
+    if (!Number.isInteger(limite) || !Number.isInteger(pagina)) {
+        response.status(Status.BAD_REQUEST).send()
+    }
+ 
+    const ITENS_POR_PAGINA = 10
+ 
+    limite = limite > ITENS_POR_PAGINA || limite <= 0 ? ITENS_POR_PAGINA : limite
+    pagina = pagina <= 0 ? 0 : pagina * limite
+ 
+    Spoiler.findAll({ limit: limite, offset: pagina }).then((spoilers) => {
+        if (spoilers && spoilers.length) {
+            response.send(spoilers)
+        } else {
+            response.status(Status.NOT_FOUND).send()
+        }
+    }).catch((error) => next(error))
+}
+ 
+// metodo para excluir dados do banco de dados
+exports.criar = (request, response, next) => {
+    const titulo = request.body.titulo
+    const espoliador = request.body.espoliador
+    const descricao = request.body.descricao
+ 
+    Spoiler.create({
+        titulo: titulo,
+        espoliador: espoliador,
+        descricao: descricao
+    }).then(() => {
+        response.status(Status.CREATED).send()
+    }).catch((error) => next(error))
+}
+//  metodo para atualizar dados dos registros
+exports.atualizar = (request, response, next) => {
+    const id = request.params.id
+    
+    const titulo = request.body.titulo
+    const espoliador = request.body.espoliador
+    const descricao = request.body.descricao
+    
+    Spoiler.findById(id).then((spoiler) => {
+        if (spoiler) {
+            Spoiler.update({
+                titulo: titulo,
+                espoliador: espoliador,
+                descricao: descricao
+            }, { where: { id: id } }).then(() => {
+                response.send()
+            }).catch((error) => next(error))
+        } else {
+            response.status(Status.NOT_FOUND).send()
+        }
+    }).catch((error) => next(error))
+}
+
+//  metodo para excluir dados dos registros
+exports.excluir = (request, response, next) => {
+    const id = request.params.id
+ 
+    Spoiler.findById(id).then((spoiler) => {
+        if (spoiler) {
+            Spoiler.destroy({
+                where: { id: id }
+            }).then(() => {
+                response.send()
+            }).catch((error) => next(error))
+        } else {
+            response.status(Status.NOT_FOUND).send()
+        }
+    }).catch((error) => next(error))
+}
